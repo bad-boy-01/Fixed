119373ea79a759470d4b936021cf6c812c73265e
Mob Spawn Notifier <animeforeveryoneandall@gmail.com>
2026-06-24 21:05:56 +0530

    Remove test and debug files from GitHub tracking

--- debug_files.py
+++
@@ -1,9 +0,0 @@
-import os
-from core.project_manager import ProjectManager
-
-pm = ProjectManager(os.getcwd(), "novel")
-print(f"Base dir: {os.getcwd()}")
-print(f"Project dir: {pm.project_dir}")
-print(f"Input dir: {pm.dirs['input']}")
-print(f"Files in input dir: {os.listdir(pm.dirs['input'])}")
-print(f"Filtered input files: {pm.get_input_files()}")
